f8549707399b9d02eb27811530cefe69811319f2
Dan Ingenthron <ingen78@gmail.com>
2018-03-06 20:03:10 -0600

    Add table to frontend

--- frontend.rb
+++ frontend.rb
@@ -1,4 +1,5 @@
 require "unirest"
+require "tty-table"
 
 system "clear"
 
@@ -6,6 +7,7 @@ puts "Welcome to World Records!!"
 puts "Choose from one of the following options:"
 puts "[1] View all available albums"
 puts "[2] View an album at random"
+puts "[3] Check them out in a table"
 
 input = gets.chomp
 
@@ -17,4 +19,11 @@ elsif input == "2"
   response = Unirest.get("http://localhost:3000/random_product")
   product = response.body
   puts JSON.pretty_generate(product)
+elsif input == "3"
+  response = Unirest.get("http://localhost:3000/all_products")
+  products = response.body
+  table = TTY::Table.new [['id','artist','title','media','price']]
+  products.each { |product|
+    table << [product["id"],product["artist"],product["title"],product["media"],product["price"]] }
+  puts table.render(:ascii)
 end
