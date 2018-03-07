af9baa580490b15dbc7b22b87d6f3d441cf1119e
Dan Ingenthron <ingen78@gmail.com>
2018-03-06 18:44:02 -0600

    Add product model

---
+++ db/migrate/20180307004326_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :artist,
+      t.string :title,
+      t.string :media,
+      t.integer, :price
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
