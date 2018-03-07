8a9074a30556d8dc27744ac02685d2916bb065fb
Dan Ingenthron <ingen78@gmail.com>
2018-03-06 19:09:00 -0600

    Seed database with 4 products

--- db/migrate/20180307004326_create_products.rb
+++ db/migrate/20180307004326_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.string :artist,
-      t.string :title,
-      t.string :media,
-      t.integer, :price
+      t.string :artist
+      t.string :title
+      t.string :media
+      t.integer :price
       t.string :image_url
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(artist: "John Coltrane", title: "Meditations", media: "LP", price: 30, image_url: "https://www.discogs.com/John-Coltrane-Meditations/release/410594")
+Product.create(artist: "Led Zeppelin", title: "Led Zeppelin II", media: "LP", price: 40, image_url: "https://www.discogs.com/Led-Zeppelin-Led-Zeppelin-II/release/3092359")
+Product.create(artist: "Pearl Jam", title: "Ten", media: "CD", price: 15, image_url: "https://www.discogs.com/Pearl-Jam-Ten/release/707653")
+Product.create(artist: "Radiohead", title: "The Bends", media: "CD", price: 15, image_url: "https://www.discogs.com/Radiohead-The-Bends/master/17008")
